82afb6f9719ab71e76d5b27c9aff2f0506ccc74e
LAVINSE <freedomsewon@gmail.com>
2023-09-06 19:24:29 +0900

    Add Script
    Add

---
+++ Assets/Scripts/Enemy/EnemyState.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyState : MonoBehaviour
+{
+    #region 변수
+    #endregion // 변수
+
+    #region 함수
+    public virtual void EnemyWait()
+    {
+
+    }
+
+    public virtual void EnemySelectPeturn()
+    {
+
+    }
+
+    public virtual void AttackReady()
+    {
+
+    }
+
+    public virtual void StartPeturn()
+    {
+
+    }
+    #endregion // 함수
+}
